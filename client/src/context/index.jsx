eac4af3ecb4ec0ed0aa38b0cc1d5e94808c5f0a3
Syeda Afsa <tamimafsa7@gmail.com>
2024-01-07 16:42:58 +0530

    Initial commit

---
+++ client/src/context/index.jsx
@@ -0,0 +1,70 @@
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import { useNavigate } from 'react-router-dom';
+
+import {ABI, ADDRESS} from '../contract';
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) =>{
+
+    const [walletAddress, setWalletAddress] = useState('');
+    const [contract, setContract] = useState('');
+    const [provider, setProvider] = useState('');
+    const [showAlert, setShowAlert] = useState({ status: false, type: 'info', message: '' });
+
+
+//* Set the wallet address to the state
+const updateCurrentWalletAddress = async () => {
+    const accounts = await window?.ethereum?.request({ method: 'eth_Accounts' });
+
+    if (accounts) setWalletAddress(accounts[0]);
+  }
+      useEffect(() => {
+        updateCurrentWalletAddress();
+
+        window?.ethereum?.on('accountsChanged', updateCurrentWalletAddress);
+      }, []);
+//* Set the smart contract and provider to the state
+      useEffect(() => {
+        const setSmartContractAndProvider = async () => {
+          const web3Modal = new Web3Modal({});
+          const connection = await web3Modal.connect();
+          const newProvider = new ethers.providers.Web3Provider(connection);
+          const signer = newProvider.getSigner();
+          const newContract = new ethers.Contract(ADDRESS, ABI, signer);
+
+          setProvider(newProvider);
+          setContract(newContract);
+    
+        }
+    
+        const timer = setTimeout(()=>setSmartContractAndProvider(),[1000]);
+
+        return () => clearTimeout(timer);
+      }, []);
+
+      useEffect(() => {
+        if (showAlert?.status) {
+          const timer = setTimeout(() => {
+            setShowAlert({ status: false, type: 'info', message: '' });
+          }, [5000]);
+    
+          return () => clearTimeout(timer);
+        }
+      }, [showAlert]);
+
+
+    return(
+      <GlobalContext.Provider value ={{      
+       contract,walletAddress, showAlert,setShowAlert
+      }}>
+        {children}
+      </GlobalContext.Provider>
+
+
+    )
+    }
+
+export const useGlobalContext = () => useContext(GlobalContext);
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import { useGlobalContext } from '../context';
+import { PageHOC, CustomInput, CustomButton } from '../components';
+
+
+const Home = () => {
+  const { contract , walletAddress, setShowAlert } = useGlobalContext();
+  const [playerName, setPlayerName] = useState('');
+
+  const handleClick = async() => {
+    try{
+      console.log({contract})
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if(!playerExists){
+        await contract.registerPLayer(playerName, playerName);
+
+        setShowAlert({
+          status: 'true',
+          type: "info",
+          message: `${playerName} is being summoned!`,
+        })
+      }
+    }catch(error){
+      alert(error);
+    }
+
+  }
+
+  return (
+    <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
+
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>,
+);
