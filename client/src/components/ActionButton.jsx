331353bc3299d8214af02747b22a01d8378c0b13
Syeda Afsa <tamimafsa7@gmail.com>
2024-02-21 21:58:45 +0530

    battle page and game info page

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const ActionButton = () => {
-  return (
-    <div>
-      Action button
-    </div>
-  )
-}
+import styles from '../styles';
 
-export default ActionButton
+const ActionButton = ({ imgUrl, handleClick, restStyles }) => (
+  <div
+    className={`${styles.gameMoveBox} ${styles.flexCenter} ${styles.glassEffect} ${restStyles} `}
+
+  >
+    <img src={imgUrl} alt="action_img" className={styles.gameMoveIcon} />
+  </div>
+);
+
+export default ActionButton;
